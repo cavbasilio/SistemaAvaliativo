d7b2e060afcfa447fd9a3f57c2b86647db87e430
Jordan <86751624+cavbasilio@users.noreply.github.com>
2023-07-23 23:29:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Avaliativo</title>
+
+    <!-- BOOTSTRAP-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css"/>  
+            
+</head>
+<body>
+
+<!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/312860882_567125738551129_7490135894950538800_n.png" id="imgplayfibra" alt="">
+  </a>
+  
+</nav>
+<h2 id="tituloPlay">PLAYFIBRA - SUPERVISÃO</h2>
+    <form action="">
+    <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+        <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+</div>
+<div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+      <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+      <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+  </div>
+</div>    <div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+        <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+</div>
+<div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+      <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="img/boneco.png">
+    <div class="card-body">
+      <h5 class="card-title">TESTE TÍTULO</h5>
+      <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
+      <div class="form-group">
+      <label id="textAvaliacao">Avaliação</label>
+      <select class="form-control" id="sel1">
+      <option selected>Escolha a nota</option>
+        <option>Ruim</option>
+        <option>Média</option>
+        <option>Bom</option>
+        <option>Ótimo</option>
+        <option>Excelente</option>
+      </select>
+    </div>
+    </div>
+  </div>
+</div>
+    </form>
+</body>
+</html>
